ae8fd269e885f9448ef9287b9972a866c3ac9b9a
moneyj1 <moneyjerome1@gmail.com>
2023-05-13 16:30:35 +0300

    git commit -a "new commits"

--- main.py
+++ main.py
@@ -7,5 +7,8 @@ with open('home.html', 'r') as html_file:
     ids = soup.find('h2')
     ida = soup.find('h1')
     jk = soup.find_all('h5')
+    nj = soup.find_all('h5', class_='hm')
     for txt in jk:
         print(txt.text)
+
+    print(nj)
