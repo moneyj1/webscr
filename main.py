3d2a330f2781e590642fe0450879e258e72a1781
moneyj1 <moneyjerome1@gmail.com>
2023-05-13 16:14:50 +0300

    commit ya first

---
+++ main.py
@@ -0,0 +1,11 @@
+from bs4 import BeautifulSoup as bs
+
+with open('home.html', 'r') as html_file:
+    content = html_file.read()
+
+    soup = bs(content, 'lxml')
+    ids = soup.find('h2')
+    ida = soup.find('h1')
+    jk = soup.find_all('h5')
+    for txt in jk:
+        print(txt.text)
